19f2ba85a61254e3a2ec2b65858318e8afa2720e
Laurie Anderson <plipandfirt@gmail.com>
2018-12-06 22:05:18 -0500

    homework Advanced JavaScript Assignment: Constructor Word Guess

---
+++ index.js
@@ -0,0 +1,23 @@
+var Word = require("./word.js");
+
+var firstWord = new Word("JavaScript");
+
+// firstWord.addWord"JSON");
+
+// firstLetter.addLetter();
+
+console.log(firstWord);
+
+// * **index.js**: The file containing the logic for the course of the game, which depends on `Word.js` and:
+
+//   * Randomly selects a word and uses the `Word` constructor to store it
+
+//   * Prompts the user for each guess and keeps track of the user's remaining guesses
+
+// 3. `Letter.js` *should not* `require` any other files.
+
+// 4. `Word.js` *should only* require `Letter.js`
+
+// 5. **HINT:** Write `Letter.js` first and test it on its own before moving on, then do the same thing with `Word.js`
+
+// 6. **HINT:** If you name your letter's display function `toString`, JavaScript will call that function automatically whenever casting that object to a string (check out this example: <https://jsbin.com/facawetume/edit?js,console>)
